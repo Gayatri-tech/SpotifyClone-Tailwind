e2e685d91d6335ddd3d22c3e4ce568d414a6d8b7
Gayu <gayatri28rawat@gmail.com>
2024-04-18 14:46:09 +0530

    functionality added

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let currentSong = new Audio();
+let play = document.getElementById("play");
+
+function secondsToMinutesSeconds(seconds) {
+  // Convert floating-point seconds to an integer
+  seconds = Math.floor(seconds);
+
+  // Calculate minutes and seconds
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  // Format minutes and seconds with leading zeros if necessary
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+  // Combine minutes and seconds in the "mm:ss" format
+  const formattedTime = `${formattedMinutes}:${formattedSeconds}`;
+
+  return formattedTime;
+}
+
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:5500/songs/");
   let response = await a.text();
@@ -14,11 +35,22 @@ async function getSongs() {
   return songs;
 }
 
-let playBtn = document.querySelector(".playBtn");
+const playMusic = (track) => {
+  // let audio = new Audio("/songs/" + track);
+  currentSong.src = "/songs/" + track;
+  currentSong.play();
+  play.src = "Media/pauseBtn.svg";
+  document.querySelector(".songInfo").innerHTML = "track";
+  // document.querySelector(".songTime").innerHTML = `${secondsToMinutesSeconds(
+  //   currentSong.currentTime
+  // )}/${secondsToMinutesSeconds(currentSong.duration)}`;
+};
+
 async function main() {
   //get the list of all songs
   let songs = await getSongs();
-  console.log(songs);
+
+  //show all the songs in playlist library
   let songsList = document
     .querySelector(".songsList")
     .getElementsByTagName("ol")[0];
@@ -39,13 +71,34 @@ async function main() {
   </li>`;
   }
 
-  //play the first song
-  var audio = new Audio(songs[0]);
-  playBtn.addEventListener("click", () => {
-    // audio.play();
+  //attach event listener to each song
+  Array.from(
+    document.querySelector(".songsList").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", () => {
+      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+    });
   });
-  audio.addEventListener("loadeddata", () => {
-    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+
+  //attach event listener to play,next and previous buttons
+  play.addEventListener("click", () => {
+    if (currentSong.paused) {
+      currentSong.play();
+      play.src = "Media/pauseBtn.svg";
+    } else {
+      currentSong.pause();
+      play.src = "Media/playBtn.svg";
+    }
+  });
+
+  //listen for timeupdate event
+  currentSong.addEventListener("timeupdate", () => {
+    console.log(currentSong.currentTime, currentSong.duration);
+    document.querySelector(".songTime").innerHTML = `${secondsToMinutesSeconds(
+      currentSong.currentTime
+    )}/${secondsToMinutesSeconds(currentSong.duration)}`;
   });
 }
+
 main();
